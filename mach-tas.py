9285323fd9ab10ee90ce8121e5a875b496fa9313
CapnJackrabbit <acemustang@live.com>
2023-09-25 13:57:14 -0300

    Versão com opção de repetir o cálculo

--- mach-tas.py
+++ mach-tas.py
@@ -1,13 +1,18 @@
 import math
 
-try:
-    mach = float(input('Entre com o número Mach: '))
-    if mach <= 0:
-        raise ValueError
-    temperatura = float(input('Entre com a temperatura: '))
-    tas = (39 * (math.sqrt(temperatura + 273))) * mach
+while True:
+    try:
+        mach = float(input('Entre com o número Mach: '))
+        if mach <= 0:
+            raise ValueError
+        temperatura = float(input('Entre com a temperatura: '))
+        tas = (39 * (math.sqrt(temperatura + 273))) * mach
 
-except ValueError:
-    print('O valor informado é inválido.')
+    except ValueError:
+        print('O valor informado é inválido.')
 
-print('True Airspeed: {:.2f} nós'.format(tas))
+    print('True Airspeed: {:.2f} nós'.format(tas))
+
+    replay = input('Deseja calcular novamente? (N) para sair: ')
+    if replay == 'n' or replay == 'N':
+        break
